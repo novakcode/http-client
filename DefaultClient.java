516bf4c8b44a13c27e14e4066092df5be9b38474
Novak Saljic <novaksaljic@gmail.com>
2021-01-09 23:08:27 +0100

    Add files via upload

---
+++ DefaultClient.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class DefaultClient extends Client{
+
+	@Override
+	public Response performRequest(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
