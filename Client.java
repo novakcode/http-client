51ff9c1ce540da6a0178912c18bc8a55967f956b
Novak Saljic <novaksaljic@gmail.com>
2021-01-12 20:09:18 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,7 +2,17 @@ package demo;
 
 public abstract class Client implements RequestHandler {
 
-	
+	private ClientConfig clientConfig;
+
+	public Client(ClientConfig config) {
+		this.clientConfig = config;
+	}
+
+	public Client() {
+		this(ClientConfig.defaultConfig());
+		HttpURLConnection con ;
+		
+	}
 
 	 
 	
